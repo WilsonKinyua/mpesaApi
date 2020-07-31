106ca20c2eefb968082a170fb8d6e8ffd9140d44
WilsonKinyua <wilsonkinyuam@gmail.com>
2020-08-01 00:35:46 +0300

    mpesaApi

---
+++ validation_url.php
@@ -0,0 +1,27 @@
+<?php
+
+	header("Content-Type: application/json");
+
+	$response = '{
+				"ResultCode": 0, 
+				"ResultDesc": "Confirmation Received Successfully"
+		}';
+
+		// DATA
+		$mpesaResponse = file_get_contents('php://input');
+
+		// log the response
+		$logFile = "M_PESAConfirmationResponse.txt";
+		$jsonMpesaResponse = json_decode($mpesaResponse, true);
+
+		// write to file
+		$log = fopen($logFile, "a");
+
+		fwrite($log, $mpesaResponse);
+		fclose($log);
+
+		echo $response;
+
+
+
+	?>
